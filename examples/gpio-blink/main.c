75c4b22ae999afb396b3eb574a8a423d696d7eb2
Laudes Rivas <laudesrivas@gmail.com>
2026-06-10 22:56:12 +0300

    gpio: fix format to match clang format
    Signed-off-by: Laudes Rivas <laudesrivas@gmail.com>

--- examples/gpio-blink/main.c
+++ examples/gpio-blink/main.c
@@ -8,21 +8,19 @@
  * @note The actual time depends on MCU clock and compiler optimizations.
  */
 static void dummy_wait(uint32_t iterations) {
-    for (volatile uint32_t i = 0; i < iterations; i++) {
-        __asm__("nop"); // Optional: prevent compiler reordering
-    }
+  for (volatile uint32_t i = 0; i < iterations; i++) {
+    __asm__("nop"); // Optional: prevent compiler reordering
+  }
 }
 
 int main(void) {
-    // LEDs are outputs from the board
-    GPIO_Init(GPIO_PORT_B, USER_LED, GPIO_MODE_OUTPUT, GPIO_NOPULL);
+  // LEDs are outputs from the board
+  GPIO_Init(GPIO_PORT_B, USER_LED, GPIO_MODE_OUTPUT, GPIO_NOPULL);
 
-    while (1)
-    {
-        GPIO_WritePin(GPIO_PORT_B, USER_LED, GPIO_PIN_SET);
-        dummy_wait(100000);
-        GPIO_WritePin(GPIO_PORT_B, USER_LED, GPIO_PIN_RESET);
-        dummy_wait(100000);
-    }
-    
+  while (1) {
+    GPIO_WritePin(GPIO_PORT_B, USER_LED, GPIO_PIN_SET);
+    dummy_wait(100000);
+    GPIO_WritePin(GPIO_PORT_B, USER_LED, GPIO_PIN_RESET);
+    dummy_wait(100000);
+  }
 }
